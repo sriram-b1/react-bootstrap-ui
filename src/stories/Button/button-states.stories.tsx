6f6d1c9d1820d8f3ed0e8433bb30cd3966cf7554
sriram_b1 <sriram_b1@dell.com>
2022-01-24 12:14:29 +0530

    Added hover, active, focus and disable state for warning, success and danger buttons

--- src/stories/Button/button-states.stories.tsx
+++ src/stories/Button/button-states.stories.tsx
@@ -29,6 +29,26 @@ storiesOf("POC2/Button States", module)
                 <Col> <Button variant="light">Tertiary</Button> </Col>
                 <Col> <Button variant="light" disabled>Tertiary</Button> </Col>
             </Row>
-
+            <Row>
+                <Col>Warning</Col>
+            </Row>
+            <Row>
+                <Col> <Button variant="warning">Warning</Button> </Col>
+                <Col> <Button variant="warning" disabled>Warning</Button> </Col>
+            </Row>
+            <Row>
+                <Col>Success</Col>
+            </Row>
+            <Row>
+                <Col> <Button variant="success">Success</Button> </Col>
+                <Col> <Button variant="success" disabled>Success</Button> </Col>
+            </Row>
+            <Row>
+                <Col>Danger</Col>
+            </Row>
+            <Row>
+                <Col> <Button variant="danger">Danger</Button> </Col>
+                <Col> <Button variant="danger" disabled>Danger</Button> </Col>
+            </Row>
         </Container>
     ))
